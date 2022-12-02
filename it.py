59fdaa3ca5dde2754e12d06a63f8420d5d4c4e6d
GeilerGuenther69420 <j.roeseler@live.de>
2022-12-02 10:43:45 +0100

    djpiwjhpoipdwoij

--- it.py
+++ it.py
@@ -1,35 +1,16 @@
-<<<<<<< HEAD
 class Anwendungsentwickler():
 
-    def __init__(self, name, lieblingsSprache, jahreErfahrung):
-        self.__name = name
-        self.__lieblingsSprache = lieblingsSprache
-        self.__jahreErfahrung = jahreErfahrung
-=======
-class Systemintegrator():
-    def __init__(self, name: str, alter: int, jahreErfahrung: int):
+    def __init__(self, name, lieblingsSprache, alter, jahreErfahrung):
         self.__name = name
         self.__alter = alter
+        self.__lieblingsSprache = lieblingsSprache
         self.__jahreErfahrung = jahreErfahrung
 
     def getName(self):
         return self.__name
-
-    def getAlter(self):
-        return self.__alter
-
-    def getJahreErfahrung(self):
-        return self.__alter
-
-    def setAlter(self, alter: int):
-        self.__alter = alter
-
-    def setName(self, name: str):
+    
+    def setName(self, name)
         self.__name = name
 
-    def setJahreErfahrung(self, jahreErfahrung: int):
-        self.__jahreErfahrung = jahreErfahrung
-
-    def __str__(self):
-        return f'Name: {self.getName()}\nAlter: {self.getAlter()}\nErfahrung in Jahren: {self.getJahreErfahrung()}\n'
->>>>>>> 492258f94609582423138cbe0ad09de357db1b66
+    def getAlter(self):
+        pass
