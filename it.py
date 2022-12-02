5b3aff8ef717df2f069e74f5a6ac490c805e48f0
GeilerGuenther69420 <j.roeseler@live.de>
2022-12-02 10:39:33 +0100

    Wo geld

---
+++ it.py
@@ -0,0 +1,6 @@
+class Anwendungsentwickler():
+
+    def __init__(self, name, lieblingsSprache, jahreErfahrung):
+        self.__name = name
+        self.__lieblingsSprache = lieblingsSprache
+        self.__jahreErfahrung = jahreErfahrung
